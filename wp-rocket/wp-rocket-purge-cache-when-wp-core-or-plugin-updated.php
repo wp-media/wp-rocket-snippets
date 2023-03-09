282072184282fd8c8c84c39108d14af61230da5a
Joe DiSalvo <joedisalvo04@gmail.com>
2023-03-09 15:55:01 -0500

    removing plugin-related code

--- wp-rocket/wp-rocket-purge-cache-when-wp-core-or-plugin-updated.php
+++ wp-rocket/wp-rocket-purge-cache-when-wp-core-or-plugin-updated.php
@@ -1,20 +1,4 @@
 <?php
-/**
- * Plugin Name: WP Rocket | Purge Cache when WP Core or Plugin Updated
- * Description: Purges WP Rocket’s cache when WP core or a plugin is updated.
- * Plugin URI:  
- * Author:      WP Rocket Support Team
- * Author URI:  http://wp-rocket.me/
- * License:     GNU General Public License v2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- *
- * Copyright SAS WP MEDIA 2023
- */
-
-namespace WP_Rocket\Helpers\cache\purge_cache_on_core_plugin_update;
-
-// Standard plugin security, keep this line in place.
-defined( 'ABSPATH' ) or die();
 
 // Clear cache when a plugin update completes
 add_action( 'upgrader_process_complete', function() {
