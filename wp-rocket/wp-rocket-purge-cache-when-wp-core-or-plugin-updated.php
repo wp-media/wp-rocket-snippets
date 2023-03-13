309cdd330ccce20cac7a1bc1f323c3e03bd768c8
Joe DiSalvo <joedisalvo04@gmail.com>
2023-03-13 13:03:26 -0400

    adding check to prevent errors when WP Rocket is disabled

--- wp-rocket/wp-rocket-purge-cache-when-wp-core-or-plugin-updated.php
+++ wp-rocket/wp-rocket-purge-cache-when-wp-core-or-plugin-updated.php
@@ -1,11 +1,13 @@
 <?php
 
-// Clear cache when a plugin update completes
-add_action( 'upgrader_process_complete', function() {
-  rocket_clean_domain();
-});
+if ( function_exists( 'rocket_clean_domain' ) ) {
+  // Clear cache when a plugin update completes
+  add_action( 'upgrader_process_complete', function() {
+    rocket_clean_domain();
+  });
 
-// Clear cache when update to WP Core completes
-add_action( '_core_updated_successfully', function() {
-  rocket_clean_domain();
-});
+  // Clear cache when update to WP Core completes
+  add_action( '_core_updated_successfully', function() {
+    rocket_clean_domain();
+  });
+}
