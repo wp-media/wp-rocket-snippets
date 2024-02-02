b5c7f430f6df3e6ad3bbde4aaa741759dff49487
Marcos Vinicios Barreto <marcc3d@gmail.com>
2024-02-02 18:13:32 -0300

    Added the snippet to export settings when it is not possible to access the tools tab in WP Rocket

---
+++ wp-rocket/rocket-export-options.php
@@ -0,0 +1,15 @@
+<?php
+// Load WordPress.
+require( 'wp-load.php' );
+
+$filename = sprintf( 'wp-rocket-settings-%s-%s.json', date( 'Y-m-d' ), uniqid() );
+//$gz       = 'gz' . strrev( 'etalfed' );
+$options  = wp_json_encode( get_option( 'wp_rocket_settings' ) ); // do not use get_rocket_option() here.
+nocache_headers();
+@header( 'Content-Type: application/json' );
+@header( 'Content-Disposition: attachment; filename="' . $filename . '"' );
+@header( 'Content-Transfer-Encoding: binary' );
+@header( 'Content-Length: ' . strlen( $options ) );
+@header( 'Connection: close' );
+echo $options;
+exit();
