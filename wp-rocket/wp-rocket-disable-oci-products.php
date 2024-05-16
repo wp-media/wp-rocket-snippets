42f92635c3aca330d32877b3dccce73c293f7d67
Alfonso Catron <alfonso.catron@gmail.com>
2024-05-16 15:01:18 -0300

    Update wp-rocket-disable-oci-products.php

--- wp-rocket/wp-rocket-disable-oci-products.php
+++ wp-rocket/wp-rocket-disable-oci-products.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: WP Rocket | Disable Optimize Critical Images for WooCommerce Products
- * Description: Disables Optimize Critical Images on WooCommerce products pages
+ * Plugin Name: WP Rocket | Disable Optimize Critical Images for specific post types
+ * Description: Disables Optimize Critical Images on specific post types
  * Author:      WP Rocket Support Team
  * Author URI:  http://wp-rocket.me/
  * License:     GNU General Public License v2 or later
@@ -15,19 +15,18 @@ namespace WP_Rocket\Helpers\compat\wc_products_no_oci;
 // Standard plugin security, keep this line in place.
 defined( 'ABSPATH' ) or die();
 
-function no_atf_for_products() {
-
-	// Make sure is_product is present.
-	if (! function_exists( 'is_product' )) {
-		return false;
-	}
-
-	/**
-	 * Disable OCI optimization on:
-	 * - single product pages
-	 */
-	if( is_product() ) {	
-		add_filter( 'rocket_above_the_fold_optimization', '__return_false' );
-	}
-}
-add_filter( 'wp',  __NAMESPACE__ .'\no_atf_for_products' );
+add_filter( 'rocket_above_the_fold_optimization', function( $enabled ) {
+	
+	// Disable Optimize Critical Images on a specific post type, using is_singular()
+	// https://developer.wordpress.org/reference/functions/is_singular/
+	//
+	// Examples: 
+	// is_singular('page'); for pages
+	// is_singular('post'); por posts
+	 // is_singular('product'); por WooCommerce products
+	 // is_singular('book'); for a custom post type called book
+	 // is_singular( array( 'product', 'post' ) ); for both, products and posts
+	 
+	return $enabled && ! is_singular( 'product' );
+	
+} );
