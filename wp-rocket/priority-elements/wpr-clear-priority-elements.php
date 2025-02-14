4e5f81121e0ac19f0a84fc7959aff1fa9af86b78
Lucy Beer <lucy@webtrainingwheels.com>
2025-02-14 12:03:28 -0800

    Create wpr-clear-priority-elements.php (#8)
    A snippet to be used to programmatically clear the priority elements data, and clear the cache. props Johan!

---
+++ wp-rocket/priority-elements/wpr-clear-priority-elements.php
@@ -0,0 +1,19 @@
+<?php
+
+// Clear Priority Elements
+
+if (defined('WP_ROCKET_VERSION')) {
+  // access rocket's injection container
+  $container = apply_filters('rocket_container', null);
+
+  // Get the Performance Hints subscriber from the container
+  $perfhints_subscriber = $container -> get('performance_hints_admin_subscriber');
+  // call the Performance Hints truncate tables method.
+  $perfhints_subscriber -> truncate_tables();
+
+}
+
+// Clear cache
+if (function_exists('rocket_clean_domain')) {
+  rocket_clean_domain();
+}
