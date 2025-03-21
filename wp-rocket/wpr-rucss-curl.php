34484d7992bf0401e7231fbf92782b80f797e9b6
Alfonso Catron <alfonso.catron@gmail.com>
2025-03-21 16:16:55 -0300

    Fix the CURL request and prettier response

--- wp-rocket/wpr-rucss-curl.php
+++ wp-rocket/wpr-rucss-curl.php
@@ -4,16 +4,16 @@
 //
 // Upload this PHP file in the public_html folder. 
 // EDIT the following fields and load it in the browser
-$url= 'https://example.com/?nowprocket';
-$wpr_email = 'your@email.com';
-$wpr_api_key = 'yourkey';
 
+$url= 'https://www.site.com/?nowprocket';
+$wpr_email = 'email@license.com';
+$wpr_api_key = 'XXXXXXXX';
 
 
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://saas.wp-rocket.me/api',
+  CURLOPT_URL => 'https://saas.wp-rocket.me/api/',
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
@@ -21,10 +21,17 @@ curl_setopt_array($curl, array(
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS => array('url' => $url,'config[rucss_safelist][]' => '','credentials[wpr_email]' => $wpr_email ,'credentials[wpr_key]' => $wpr_api_key,'config[treeshake]' => '1','config[is_home]' => '0','config[is_mobile]' => ''),
+  CURLOPT_POSTFIELDS => array('url' => $url ,'credentials[wpr_email]' => $wpr_email ,'credentials[wpr_key]' => $wpr_api_key,'config[treeshake]' => '1','config[is_home]' => '1'),
+  CURLOPT_HTTPHEADER => array(
+    'User-Agent: '
+  ),
 ));
 
 $response = curl_exec($curl);
 
 curl_close($curl);
+
+echo '<pre>';
 echo $response;
+echo '</pre>';
+
